27519a3adb95c140a3ab53d177831983d1a68886
Mortell560 <62893400+Mortell560@users.noreply.github.com>
2024-05-03 17:06:25 +0200

    Update Competition.sql
    New names

--- SQL_scripts/Competition.sql
+++ SQL_scripts/Competition.sql
@@ -1,2 +1,2 @@
-INSERT INTO Competition VALUES (1, 'RBA', 480, '2024-01-20 00:00:00',1, 32, 4, 8);
-INSERT INTO Competition VALUES (2, 'Hands off', 240, '2024-01-20 00:00:30',2, 40, 4, 10);
+INSERT INTO Competition VALUES (1, 'Basketball', 480, '2024-01-20 00:00:00',1, 32, 4, 8);
+INSERT INTO Competition VALUES (2, 'Hand''s off', 240, '2024-01-20 00:00:30',2, 40, 4, 10);
