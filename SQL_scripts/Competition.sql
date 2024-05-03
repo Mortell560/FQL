3e15f75f31b2d88414e6b049fcacee9da6e9fa26
Mortell560 <62893400+Mortell560@users.noreply.github.com>
2024-05-03 17:32:39 +0200

    Update Competition.sql
    Je ne modifie plus ce fichier, vous le faites

--- SQL_scripts/Competition.sql
+++ SQL_scripts/Competition.sql
@@ -1,2 +1,12 @@
-INSERT INTO Competition VALUES (1, 'Basketball', 480, '2024-01-20 00:00:00',1, 32, 4, 8);
-INSERT INTO Competition VALUES (2, 'Hand''s off', 240, '2024-01-20 00:00:30',2, 40, 4, 10);
+-- Mettre les orga avant et savoir leurs id, ca va etre important
+INSERT INTO Competition VALUES (1, 'Ball Busters', 2760, '2024-06-22 14:00:00', 1, 32, 4, 8);
+INSERT INTO Competition VALUES (2, 'Hand''s off', 3120, '2024-06-20 08:00:00', 2, 40, 4, 10);
+INSERT INTO Competition VALUES (3, 'Soccer Royale', 3240 '2024-06-24 14:00:00', 1, 24, 3, 12);
+INSERT INTO Competition VALUES (4, 'Lancer de troncs d''arbre', 960, '2024-06-20 08:00:00', 3, 10, 1, 10);
+INSERT INTO Competition VALUES (5, 'Porter de femme', 960, '2024-06-21 08:00:00', 3, 10, 1, 10);
+INSERT INTO Competition VALUES (6, 'Sacathlon', 960, '2024-06-22 08:00:00', 3, 10, 1, 10);
+INSERT INTO Competition VALUES (7, 'Breakdance', 2400, '2024-06-23 08:00:00', 2, 10, 3, 5);
+INSERT INTO Competition VALUES (8, 'Hockey sur gazon', 960,'2024-06-25 08:00:00', 3, 20, 2, 10);
+INSERT INTO Competition VALUES (9, 'Pétanque/Molkky', 960, '2024-06-25 08:00:00', 5, 20, 4, 5);
+INSERT INTO Competition VALUES (10, 'Tennis de Table', 240, '2024-06-26 08:00:00', 4, 6, 2, 2);
+INSERT INTO Competition VALUES (11, 'Judo', 360, '2024-06-26 14:00:00', 5, 6, 2, 2);
