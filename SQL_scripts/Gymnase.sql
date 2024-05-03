94536dab72167ce0fb81b3717c2aa09506bb391f
Mortell560 <62893400+Mortell560@users.noreply.github.com>
2024-05-03 15:00:07 +0200

    Update Gymnase.sql
    Added all current known gymnases

--- SQL_scripts/Gymnase.sql
+++ SQL_scripts/Gymnase.sql
@@ -2,3 +2,7 @@ INSERT INTO Gymnase VALUES (1, 'Gymnase Municipal de Mourenx', '64150 Rue Gaston
 INSERT INTO Gymnase VALUES (123, 'Gymnase InShape', '64150 Rue de Tibo, 94150 Mourenx', 150);
 INSERT INTO Gymnase VALUES (44, 'Sieges rouges', 'salle de spectable de Mou, 94150 Mourenx', 150);
 INSERT INTO Gymnase VALUES (43, 'Le mix', 'salle de spectacle de Mou, 94150 Mourenx', 150);
+INSERT INTO Gymnase VALUES (15, 'Salle principale', '16 place de la Liberté, Le Lion d''or', 100);
+INSERT INTO Gymnase VALUES (14, 'Terasse', '16 place de la Liberté, Le Lion d''or', 250);
+INSERT INTO Gymnase VALUES (41, 'Salle des fetes', 'Place du fronton 64520 Bi, Bidache', 200);
+INSERT INTO Gymnase VALUES (42, 'Salle polyvalente', 'Place du fronton 64520 Bi, Bidache', 200);
