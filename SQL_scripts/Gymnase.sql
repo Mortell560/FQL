09c50156608fe4467f58668e22fcde50fd4fe627
Mortell560 <mortell560@gmail.com>
2024-05-06 15:27:02 +0200

    added correction gymnase

--- SQL_scripts/Gymnase.sql
+++ SQL_scripts/Gymnase.sql
@@ -1,8 +1,8 @@
-INSERT INTO Gymnase VALUES (1, 'Gymnase Municipal de Mourenx', '64150 Rue Gaston de Foix, 64150 Mourenx', 250);
-INSERT INTO Gymnase VALUES (123, 'Gymnase InShape', '64150 Rue de Tibo, 94150 Mourenx', 150);
-INSERT INTO Gymnase VALUES (44, 'Sieges rouges', 'salle de spectable de Mou, 94150 Mourenx', 150);
-INSERT INTO Gymnase VALUES (43, 'Le mix', 'salle de spectacle de Mou, 94150 Mourenx', 150);
-INSERT INTO Gymnase VALUES (15, 'Salle principale', '16 place de la Liberté, Le Lion d''or', 100);
-INSERT INTO Gymnase VALUES (14, 'Terasse', '16 place de la Liberté, Le Lion d''or', 250);
-INSERT INTO Gymnase VALUES (41, 'Salle des fetes', 'Place du fronton 64520 Bi, Bidache', 200);
-INSERT INTO Gymnase VALUES (42, 'Salle polyvalente', 'Place du fronton 64520 Bi, Bidache', 200);
+--INSERT INTO Gymnase VALUES (1, 'Gymnase Municipal de Mourenx', '64150 Rue Gaston de Foix, 64150 Mourenx', 250);
+--INSERT INTO Gymnase VALUES (123, 'Gymnase InShape', '64150 Rue de Tibo, 64150 Mourenx', 150);
+INSERT INTO Gymnase VALUES (44, 'Sieges rouges', 'salle de spectable de Mou, 64150 Mourenx', 64);
+INSERT INTO Gymnase VALUES (43, 'Le mix', 'salle de spectacle de Mou, 94150 Mourenx', 92);
+INSERT INTO Gymnase VALUES (15, 'Salle principale', '16 place de la Liberté', 28);
+INSERT INTO Gymnase VALUES (14, 'Terasse', '16 place de la Liberté', 12);
+INSERT INTO Gymnase VALUES (41, 'Salle des fetes', 'Place du fronton 64520 Bidache', 80);
+INSERT INTO Gymnase VALUES (42, 'Salle polyvalente', 'Place du fronton 64520 Bidache', 120);
