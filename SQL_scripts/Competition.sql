946b2784340e85f628a353ad22fbe20270e3d03f
Mortell560 <mortell560@gmail.com>
2024-05-06 14:49:57 +0200

    corrected Competition

--- SQL_scripts/Competition.sql
+++ SQL_scripts/Competition.sql
@@ -1,7 +1,7 @@
 -- Mettre les orga avant et savoir leurs id, ca va etre important
 INSERT INTO Competition VALUES (1, 'Ball Busters', 2760, '2024-06-22 14:00:00', 1, 32, 4, 8);
 INSERT INTO Competition VALUES (2, 'Hand''s off', 3120, '2024-06-20 08:00:00', 2, 40, 4, 10);
-INSERT INTO Competition VALUES (3, 'Soccer Royale', 3240 '2024-06-24 14:00:00', 1, 24, 3, 12);
+INSERT INTO Competition VALUES (3, 'Soccer Royale', 3240, '2024-06-24 14:00:00', 1, 24, 3, 12);
 INSERT INTO Competition VALUES (4, 'Lancer de troncs d''arbre', 960, '2024-06-20 08:00:00', 3, 10, 1, 10);
 INSERT INTO Competition VALUES (5, 'Porter de femme', 960, '2024-06-21 08:00:00', 3, 10, 1, 10);
 INSERT INTO Competition VALUES (6, 'Sacathlon', 960, '2024-06-22 08:00:00', 3, 10, 1, 10);
