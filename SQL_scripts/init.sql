9ec89e0bd951dde90cb0b5f8bf70e7f27dafdd4a
Mortell560 <mortell560@gmail.com>
2024-05-07 08:56:59 +0200

    Modified schema (cf draw.io)

--- SQL_scripts/init.sql
+++ SQL_scripts/init.sql
@@ -50,11 +50,13 @@ CREATE TABLE Gymnase(
 CREATE TABLE Role (
   NumPersonne BIGINT NOT NULL,
   NumCompetition BIGINT NOT NULL,
+  NumSport BIGINT NOT NULL,
   bSportif INT DEFAULT 0 NOT NULL,
   bArbitre INT DEFAULT 0 NOT NULL,
   bOrga INT DEFAULT 0 NOT NULL,
   FOREIGN KEY (NumPersonne) REFERENCES Personne(NumPersonne),
   FOREIGN KEY (NumCompetition) REFERENCES Competition(NumCompetition),
+  FOREIGN KEY(NumSport) REFERENCES Sport(NumSport),
   PRIMARY KEY(NumPersonne, NumCompetition)
 );
 
