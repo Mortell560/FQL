465c8114906480e78cc669ca57a456b6bd1f84d3
FlowyCode5 <135832772+FlowyCode5@users.noreply.github.com>
2024-05-07 15:15:25 +0000

    changement d'horaire pour une compet' de tennis de table + ajout de retour à la ligne + ajouts de tuples dans SportCompetition

--- SQL_scripts/SportCompetition.sql
+++ SQL_scripts/SportCompetition.sql
@@ -1,1 +1,34 @@
-INSERT INTO SportCompetition VALUES (1,1); -- Placeholder
+INSERT INTO SportCompetition VALUES (1,1,43); 
+INSERT INTO SportCompetition VALUES (12,1,42);
+INSERT INTO SportCompetition VALUES (13,1,43);
+
+INSERT INTO SportCompetition VALUES (2,2,42); 
+INSERT INTO SportCompetition VALUES (14,2,42);
+INSERT INTO SportCompetition VALUES (15,2,42);
+
+INSERT INTO SportCompetition VALUES (3,3,42); 
+INSERT INTO SportCompetition VALUES (16,3,42);
+INSERT INTO SportCompetition VALUES (17,3,42);
+
+INSERT INTO SportCompetition VALUES (4,4,14);
+
+INSERT INTO SportCompetition VALUES (5,5,44);
+
+INSERT INTO SportCompetition VALUES (6,6,14);
+
+INSERT INTO SportCompetition VALUES (7,7,42);
+INSERT INTO SportCompetition VALUES (18,7,42);
+
+INSERT INTO SportCompetition VALUES (8,8,14);
+
+INSERT INTO SportCompetition VALUES (9,9,42);
+INSERT INTO SportCompetition VALUES (9,10,42);
+
+INSERT INTO SportCompetition VALUES (10,11,42);
+INSERT INTO SportCompetition VALUES (20,11,42);
+
+INSERT INTO SportCompetition VALUES (11,12,41);
+INSERT INTO SportCompetition VALUES (19,12,41);
+
+
+
