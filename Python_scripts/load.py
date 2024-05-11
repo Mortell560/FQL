97f3135aa690c52934b898483f33f37b44383e62
Mortell560 <mortell560@gmail.com>
2024-05-11 16:12:13 +0200

    bon si la on est pas pret, c'est qu'on est mort

--- Python_scripts/load.py
+++ Python_scripts/load.py
@@ -4,16 +4,16 @@ def insert(tablename: str, values: list[list]) -> str:
     for value in values:
         to_add = "("
         for v in value:
-            if isinstance(v, str):
-                v = v.replace("'", "''")
             to_add += str(v) + ","
         to_add = to_add[:-1] + ")"
         out += to_add + ", "
 
-        # les -1 c'est pour retirer les char en trop
-    return out[:-1] + ";"
+        # les -1 ou -2 c'est pour retirer les char en trop
+    return out[:-2] + ";"
 
 
 if __name__ == "__main__":
-    insert("test", [[1,'test', 3], [2, 'test2', 6]])
+    t = insert("test", [[1,'\'test\'', 3], [2, '\'test2\'', 6], [3, '\'test4\'', 1234]])
+    print(t)
 
+    # TODO: Pensez à sanatize les inputs avec les '' qui peuvent mettre le chaos 
