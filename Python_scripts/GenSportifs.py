76b4864a84984c32652db3c1b66d38e6d8b8ded7
Mortell560 <mortell560@gmail.com>
2024-05-13 18:54:14 +0200

    Petites erreurs
    f"" c'est pour du formatting où on a besoin de mettre des variables dans
    notre str sans galérer et toujours utiliser un chemin relatif pour les
    fichiers

--- Python_scripts/GenSportifs.py
+++ Python_scripts/GenSportifs.py
@@ -1,6 +1,6 @@
 import random
 
-with open("c:/Users/ambre/Documents/GitHub/FQL/SQL_scripts/Sportifs.sql", 'w') as f:
+with open("../SQL_scripts/Sportifs.sql", 'w') as f:
     sportifs_par_comp = [32,40,24,5,10,5,10,2,5,6,6,32,32,40,40,24,24,10,6,5,32]
     non_select = [1,2,3,4,5,8,10,12,14,16,18,20,21,22,27]
     for i in range(len(sportifs_par_comp)):
@@ -15,5 +15,5 @@ with open("c:/Users/ambre/Documents/GitHub/FQL/SQL_scripts/Sportifs.sql", 'w') a
                 f"INSERT INTO Role VALUES ({id}, {i+1}, 1, 0, 0);\n"
             )
         f.write(
-                f"\n"
+                "\n"
             )
