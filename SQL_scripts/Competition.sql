66fcd22d40839bfba8adecdfd1d6da749ee4340f
Cleliambre <168779349+Cleliambre@users.noreply.github.com>
2024-05-13 13:33:30 +0000

    flemme de résumer...

--- SQL_scripts/Competition.sql
+++ SQL_scripts/Competition.sql
@@ -12,16 +12,16 @@ INSERT INTO Competition VALUES (3, 'Soccer Royale', 120, '2024-06-24 14:00:00', 
 INSERT INTO Competition VALUES (16, 'Soccer Royale', 120, '2024-06-25 14:00:00', 3, 24, 3, 12);
 INSERT INTO Competition VALUES (17, 'Soccer Royale', 120, '2024-06-26 14:00:00', 1, 24, 3, 12);
 
-INSERT INTO Competition VALUES (4, 'Lancer de troncs d''arbre', 120, '2024-06-20 08:00:00', 3, 10, 1, 10);
+INSERT INTO Competition VALUES (4, 'Lancer de troncs d''arbre', 120, '2024-06-20 08:00:00', 3, 5, 1, 5);
 
 INSERT INTO Competition VALUES (5, 'Porter de femme', 120, '2024-06-21 08:00:00', 3, 10, 1, 10);
 
-INSERT INTO Competition VALUES (6, 'Sacathlon', 120, '2024-06-22 08:00:00', 3, 10, 1, 10);
+INSERT INTO Competition VALUES (6, 'Sacathlon', 120, '2024-06-22 08:00:00', 3, 5, 1, 5);
 
 INSERT INTO Competition VALUES (7, 'Breakdance', 120, '2024-06-23 08:00:00', 2, 10, 3, 5);
 INSERT INTO Competition VALUES (18, 'Breakdance', 120, '2024-06-24 08:00:00', 2, 10, 3, 5);
 
-INSERT INTO Competition VALUES (8, 'Hockey sur gazon', 120,'2024-06-25 08:00:00', 3, 20, 2, 10);
+INSERT INTO Competition VALUES (8, 'Hockey sur gazon', 120,'2024-06-25 08:00:00', 3, 2, 2, 10);
 
 INSERT INTO Competition VALUES (9, 'Pétanque', 120, '2024-06-25 08:00:00', 1, 5, 2, 3);
 INSERT INTO Competition VALUES (20, 'Molkky', 120, '2024-06-25 08:00:00', 1, 5, 2, 3);
