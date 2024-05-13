c57be3640e8a254e89f6b72f49a1b50f08bd56b9
Mortell560 <mortell560@gmail.com>
2024-05-13 13:39:17 +0200

    updated competitions

--- SQL_scripts/Competition.sql
+++ SQL_scripts/Competition.sql
@@ -1,6 +1,6 @@
 -- Mettre les orga avant et savoir leurs id, ca va etre important
 INSERT INTO Competition VALUES (1, 'Ball Busters', 120, '2024-06-22 14:00:00', 1, 32, 4, 8);
-INSERT INTO Competition VALUES (12, 'Ball Busters', 120, '2024-06-23 14:00:00', 1, 32, 4, 8);
+INSERT INTO Competition VALUES (12, 'Ball Busters', 120, '2024-06-23 14:00:00', 3, 32, 4, 8);
 INSERT INTO Competition VALUES (13, 'Ball Busters', 120, '2024-06-24 14:00:00', 1, 32, 4, 8);
 
 INSERT INTO Competition VALUES (2, 'Hand''s off', 120, '2024-06-20 08:00:00', 2, 40, 4, 10);
@@ -8,7 +8,7 @@ INSERT INTO Competition VALUES (14, 'Hand''s off', 120, '2024-06-21 08:00:00', 2
 INSERT INTO Competition VALUES (15, 'Hand''s off', 120, '2024-06-22 08:00:00', 2, 40, 4, 10);
 
 INSERT INTO Competition VALUES (3, 'Soccer Royale', 120, '2024-06-24 14:00:00', 1, 24, 3, 12);
-INSERT INTO Competition VALUES (16, 'Soccer Royale', 120, '2024-06-25 14:00:00', 1, 24, 3, 12);
+INSERT INTO Competition VALUES (16, 'Soccer Royale', 120, '2024-06-25 14:00:00', 3, 24, 3, 12);
 INSERT INTO Competition VALUES (17, 'Soccer Royale', 120, '2024-06-26 14:00:00', 1, 24, 3, 12);
 
 INSERT INTO Competition VALUES (4, 'Lancer de troncs d''arbre', 120, '2024-06-20 08:00:00', 3, 10, 1, 10);
